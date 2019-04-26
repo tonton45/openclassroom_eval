ff53e682fe8e7fae03471031a713c362c646bac4
PINET Frédéric UI NC <UFPE7441@orange.com>
2019-04-26 15:16:28 +0200

    mise en BDD du message

--- minichat_post.php
+++ minichat_post.php
@@ -8,6 +8,20 @@ if (isset($_POST['pseudo']) AND isset($_POST['message']) )
 	$message = strip_tags($_POST['message']);
 	$_SESSION['pseudo-seesion'] = $pseudo;
 
+	try
+	{
+		$bdd = new PDO('mysql:host=localhost;dbname=test_formation;charset=utf8', 'root', '');
+	}
+	catch (Exception $e)
+	{
+	        die('Erreur : ' . $e->getMessage());
+	}
+
+	$req = $bdd->prepare('INSERT INTO minichat(pseudo, message, date_message) VALUES(:pseudo, :message, NOW())');
+	$req->execute(array(
+		'pseudo' => $pseudo,
+		'message' => $message,
+		));
 
 
 }
